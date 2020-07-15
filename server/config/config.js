67683afbe6cd49644f4e55db62dbaec16d967a6a
AndresMndez <andresmndeze@gmail.com>
2020-07-14 21:08:35 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -14,9 +14,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb://localhost:27017/cafe'
+    urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://dbMendy:@IwOsQ38@cluster0-4voft.mongodb.net/cafe?retryWrites=true&w=majority'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
